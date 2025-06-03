7243a5dac496dea2267f089ea56a05836875a4de
andreeabrindea <andreeabrindea61@gmail.com>
2025-06-04 00:49:40 +0300

    feat: implement burger menu for small screens

--- app/components/NavBar/NavBar.tsx
+++ app/components/NavBar/NavBar.tsx
@@ -2,11 +2,33 @@ import './navbar.css';
 import logo from '@/public/cooking.png';
 import Image from 'next/image';
 import { FaSearch } from 'react-icons/fa';
+import { useState } from 'react';
 
 export default function NavBar() {
-    return (
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const handleHamburgerMenuClicked = () => {
+    console.log("ello?");
+    setIsMenuOpen((prev) => {
+      console.log(prev);
+      return !prev;
+    });
+  };
+  return (
+    <>
+      <div className={`off-screen-menu ${isMenuOpen ? 'active' : ''}`}>
+        <ul id="list-pages-off-screen">
+          <li>HOME</li>
+          <li>RECIPES</li>
+          <li>COOKING TIPS</li>
+          <li>ABOUT US</li>
+        </ul>
+        <section id="off-screen-menu-footer">
+          <FaSearch id="search-icon-off-screen"></FaSearch>
+          <button>Subscribe</button>
+        </section>
+      </div>
       <section id='navigation-wrapper'>
-        <Image src={logo} alt = "cooking pot" id="logo-image"/>
+        <Image src={logo} alt="cooking pot" id="logo-image" />
         <ul id="list-pages">
           <li>HOME</li>
           <li>RECIPES</li>
@@ -14,6 +36,14 @@ export default function NavBar() {
           <li>ABOUT US</li>
         </ul>
         <FaSearch id="search-icon"></FaSearch>
+        <nav id="hamburger-menu-nav">
+          <div className={`hamburger-menu ${isMenuOpen ? 'active' : ''}`} onClick={handleHamburgerMenuClicked}>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </nav>
       </section>
-    );
-  }
+    </>
+  );
+}
